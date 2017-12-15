066e9701b9da35c520ff37d44d5a6ad20873c654
yz190598886 <190598886@qq.com>
2017-12-15 15:20:47 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ bbb/src/test/java/fileone/testO.java
@@ -0,0 +1,23 @@
+package fileone;
+
+import org.testng.annotations.Test;
+
+    public class testO {
+        @Test
+        public void one(){
+            System.out.println("111111111111111111111");
+        }
+        @Test
+        public void two(){
+            System.out.println("22222222222222222222");
+        }
+        @Test
+        public void three(){
+            System.out.println("3333333333333333333333");
+        }
+        @Test
+        public void four(){
+            System.out.println("444444444444444444444444");
+        }
+    }
+
---
+++ bbb/src/test/java/flietwo/testT.java
@@ -0,0 +1,10 @@
+package flietwo;
+
+import org.testng.annotations.Test;
+
+public class testT {
+    @Test
+    public void testt(){
+        System.out.println("-----------------------");
+    }
+}
