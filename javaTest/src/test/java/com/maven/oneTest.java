12b17d52e16c78916b3b97bd3ed8845df7b93634
yz190598886 <190598886@qq.com>
2018-01-02 15:02:18 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ javaTest/src/test/java/com/maven/oneTest.java
@@ -0,0 +1,19 @@
+package com.maven;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class oneTest{
+      @Test
+      public void one(){
+         System.out.println("1111111111111111111111111111");
+  }
+      @Test
+      public void two(){
+         System.out.println("2222222222222222222222222222222");
+      }
+      @Test
+      public void three(){
+         System.out.println("33333333333333333333333333");
+      }
+}
---
+++ javaTest/src/test/java/com/maven/twoTest.java
@@ -0,0 +1,18 @@
+package com.maven;
+
+import org.junit.Test;
+
+public class twoTest {
+    @Test
+    public void one(){
+        System.out.println("3333333333333333333333");
+    }
+    @Test
+    public void two(){
+        System.out.println("4444444444444444444444");
+    }
+    @Test
+    public void three(){
+        System.out.println("55555555555555555555555555");
+    }
+}
