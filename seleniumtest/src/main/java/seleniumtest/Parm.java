c32ff2096328ef530fc75d4a0b4406f6993d5703
yz190598886 <190598886@qq.com>
2018-01-31 18:12:06 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ seleniumtest/src/main/java/seleniumtest/Parm.java
@@ -0,0 +1,32 @@
+package seleniumtest;
+
+public class Parm {
+    String Addr;
+    String Pram;
+
+    public String getExpectation() {
+        return Expectation;
+    }
+
+    public void setExpectation(String expectation) {
+        Expectation = expectation;
+    }
+
+    String Expectation;
+
+    public String getAddr() {
+        return Addr;
+    }
+
+    public void setAddr(String addr) {
+        Addr = addr;
+    }
+
+    public String getPram() {
+        return Pram;
+    }
+
+    public void setPram(String pram) {
+        Pram = pram;
+    }
+}
---
+++ seleniumtest/src/main/java/seleniumtest/testOne.java
@@ -0,0 +1,50 @@
+package seleniumtest;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class testOne {
+
+
+        private static List<Parm> list = new ArrayList<>();
+
+        public static void file() {
+
+            File file = new File("H:\\1.txt");
+
+            try {
+                BufferedReader input = new BufferedReader(new FileReader(file));
+                String text = "";
+                while ((text = input.readLine()) != null) {
+                    Parm parm = new Parm();
+                    String[] a = text.split(",");
+                    parm.setAddr(a[0]);
+                    parm.setPram(a[1]);
+                    parm.setExpectation(a[2]);
+                    list.add(parm);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(list);
+        }
+
+    public static void main(String[] args) {
+        file();
+        for (Parm a : list) {
+            // TODO
+            diaoyong(a.getAddr(), a.getPram(),a.getExpectation());
+        }
+
+    }
+
+        private static void diaoyong(String addr, String pram,String Expectation) {
+            System.out.println(addr+pram+Expectation);
+        }
+    }
+
