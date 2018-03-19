ca5eb31da899020d1b1f860e6d23ad582d76b3ad
yz190598886 <190598886@qq.com>
2018-03-19 17:55:14 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

--- TestNg/src/test/java/TestNg/AppTest.java
+++ TestNg/src/test/java/TestNg/AppTest.java
@@ -6,8 +6,45 @@ import org.testng.annotations.Test;
 public class AppTest
 {
     @Test
-    public void one(){
+    public void one0(){
 
         System.out.println("1111111111");
     }
+    @Test
+    public void one1(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one2(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one3(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one4(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one5(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one6(){
+
+        System.out.println("1111111111");
+    }
+    @Test
+    public void one7(){
+
+        System.out.println("1111111111");
+    }
+
+
 }
