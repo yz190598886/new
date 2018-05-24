227cd687fcba378d9e83eb9b17c25c0a6b909286
zhangrui <zhangrui@fkhwl.com>
2018-05-24 11:24:33 +0800

    Signed-off-by: zhangrui <zhangrui@fkhwl.com>

--- seleniumtest/src/main/java/seleniumtest/testOne.java
+++ seleniumtest/src/main/java/seleniumtest/testOne.java
@@ -1,8 +1,6 @@
 package seleniumtest;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,29 +9,23 @@ public class testOne {
 
     private static List<Parm> list = new ArrayList<>();
 
-    public static void file() {
+    public static void file() throws IOException {
+      File file=new File("H:\\1.txt");
 
-        File file = new File("H:\\1.txt");
+      BufferedReader input =new BufferedReader(new FileReader(file));
+      String text="";
+        while((text=input.readLine())!=null){
+            Parm parm=new Parm();
+            String [] a=text.split(",");
+            parm.setAddr(a[0]);
+            parm.setPramOne(a[1]);
+            parm.setPramTwo(a[2]);
+            list.add(parm);
+            System.out.println(list);
 
-        try {
-            BufferedReader input = new BufferedReader(new FileReader(file));
-            String text = "";
-            while ((text = input.readLine()) != null) {
-                Parm parm = new Parm();
-                String[] a = text.split(",");
-                parm.setAddr(a[0]);
-                parm.setPramOne(a[1]);
-                parm.setPramTwo(a[2]);
-                list.add(parm);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-        System.out.println(list);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         file();
         for (Parm a : list) {
             // TODO
@@ -43,7 +35,7 @@ public class testOne {
     }
 
         private static void diaoyong(String addr, String PramOne,String PramTwo) {
-            System.out.println(addr+"--"+PramOne+"--"+PramTwo);
+            System.out.println(addr+"   "+PramOne+"   "+PramTwo);
         }
     }
 
